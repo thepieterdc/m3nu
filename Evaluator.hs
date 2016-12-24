cc5cb97e15de57ce9842c1fa333fc09e1a9c77bd
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-12-25 00:00:12 +0100

    Puke refactor

--- Evaluator.hs
+++ Evaluator.hs
@@ -4,11 +4,11 @@ import Environment
 import Types
 
 evaluate :: Statement -> Environment -> IO Environment
-evaluate (Puke s) = evaluatePrint s
+evaluate (Puke s) = evaluatePuke s
 
 evaluateBoolExp :: BoolExp -> Environment -> IO Bool
 evaluateBoolExp (BoolConst b) _ = return b
 evaluateBoolExp (BoolNegate bExp) e = do { ex <- evaluateBoolExp bExp e; return $ not ex}
 
-evaluatePrint :: String -> Environment -> IO Environment
-evaluatePrint txt e = do { putStrLn txt; return e}
+evaluatePuke :: String -> Environment -> IO Environment
+evaluatePuke txt e = do { putStrLn txt; return e}
