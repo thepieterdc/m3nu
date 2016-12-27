50276a6d57c94d667dd255e2d617af1b9686cf95
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-12-27 18:37:23 +0100

    Working on evaluator

--- Evaluator.hs
+++ Evaluator.hs
@@ -16,10 +16,18 @@ evaluate (Puke v) = evaluatePuke v
 evaluate Review = return
 evaluate (Seq s) = evaluateSequence s
 
-evaluateArithExp :: ArithExp -> Environment -> IO Double
-evaluateArithExp (ArithConst c) _ = return c
-evaluateArithExp (ArithBinary op x y) env = evaluateArithBinaryExp op x y env
-evaluateArithExp (Variable v) env = return $ fromMaybe (error $ "Unknown variable: " ++ v) (getVariable v env)
+-- data Exp = Constant Double
+--          | Variable String
+--          | Binary BinaryOp Exp Exp
+--          | Unary UnaryOp Exp
+--          | Relational RelationalOp Exp Exp
+
+evaluateExp :: Exp -> Environment -> IO Double
+evaluateExp (Constant c) _ = return c
+evaluateExp (Variable v) env = return $ fromMaybe (error $ "Unknown variable: " ++ v) (getVariable v env)
+evaluateExp (Binary op x y) env = evaluateBinaryExp op x y env
+evaluateExp (Unary op x) env = evaluateUnaryExp op x env
+evaluateExp (Relational op x y) env = evaluateRelational op x y env
 
 evaluateArithBinaryExp :: ArithBinaryOp -> ArithExp -> ArithExp -> Environment -> IO Double
 evaluateArithBinaryExp Add x y env = do { xe <- evaluateArithExp x env; ye <- evaluateArithExp y env; return $ xe+ye}
@@ -38,33 +46,32 @@ evaluateBoolBinaryExp And x y env = do { xe <- evaluateBoolExp x env; ye <- eval
 evaluateBoolBinaryExp Or x y env = do { xe <- evaluateBoolExp x env; ye <- evaluateBoolExp y env; return $ xe || ye}
 evaluateBoolBinaryExp BoolEquals x y env = do { xe <- evaluateBoolExp x env; ye <- evaluateBoolExp y env; return $ xe == ye }
 
-evaluateCook :: ArithExp -> Environment -> IO Environment
+evaluateCook :: Exp -> Environment -> IO Environment
 evaluateCook amtexp env = do { amt <- evaluateArithExp amtexp env; _ <- threadDelay $ round $ amt*1000000; return env}
 
 evaluateDebug :: String -> Environment -> IO Environment
 evaluateDebug txt env = do { print txt; return env}
 
-evaluateEating :: BoolExp -> Statement -> Environment -> IO Environment
+evaluateEating :: Exp -> Statement -> Environment -> IO Environment
 evaluateEating cond task env = do
   loopcond <- evaluateBoolExp cond env
-  if loopcond then do
+  if loopcond /= 0 then do
     env' <- evaluate task env
     evaluateEating cond task env'
   else return env
 
-evaluateHungry :: BoolExp -> Statement -> Statement -> Environment -> IO Environment
+evaluateHungry :: Exp -> Statement -> Statement -> Environment -> IO Environment
 evaluateHungry cond ifc elsec env = do
   c <- evaluateBoolExp cond env
-  if c then evaluate ifc env
-  else evaluate elsec env
+  if c /= 0 then evaluate ifc env else evaluate elsec env
 
-evaluateOrder :: String -> ArithExp -> Environment -> IO Environment
+evaluateOrder :: String -> Exp -> Environment -> IO Environment
 evaluateOrder var val env = do
-  x <- evaluateArithExp val env
+  x <- evaluateExp val env
   return $ setVariable var x env
 
-evaluatePuke :: ArithExp -> Environment -> IO Environment
-evaluatePuke e env = do { val <- evaluateArithExp e env; print val; return env}
+evaluatePuke :: Exp -> Environment -> IO Environment
+evaluatePuke e env = do { val <- evaluateExp e env; print val; return env}
 
 evaluateRelationalExp :: RelationalBinaryOp -> ArithExp -> ArithExp -> Environment -> IO Bool
 evaluateRelationalExp Greater x y env = do { xe <- evaluateArithExp x env; ye <- evaluateArithExp y env; return $ xe > ye}
